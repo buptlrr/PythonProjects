2bd0c6d7f1769035ff66a0f37cced5cdb4b7179d
buptlrr <l007rr@bupt.edu.cn>
2014-11-15 14:48:00 +0800

    Revert "Other Web Projects"
    This reverts commit bd0be9fe5ed974145a4c79ddcb8d7cdea98feec8.
    
    Conflicts:
    	Projects/PythonWeb/hello.c

--- Projects/PythonWeb/realurl.py
+++
@@ -1,8 +0,0 @@
-from urllib2 import Request, urlopen, URLError, HTTPError
-
-
-old_url = 'http://www.weibo.com'
-req = Request(old_url)
-response = urlopen(req)
-print 'Old url :' + old_url
-print 'Real url :' + response.geturl()
