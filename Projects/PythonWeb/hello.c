85425b797dae392d0fc81dc2e983e4ab0c2de69e
buptlrr <l007rr@bupt.edu.cn>
2014-11-15 14:54:53 +0800

    aa
    aa

--- Projects/PythonWeb/hello.c
+++ Projects/PythonWeb/hello.c
@@ -5,9 +5,13 @@ int main()
 {
 	//printf("hello");
 <<<<<<< HEAD
+<<<<<<< HEAD
 	cout<<"hello and fuck and ";
 =======
 	cout<<"hello and fuck";
 >>>>>>> parent of 2d4aa52... Revert "hello.c"
+=======
+	cout<<"hello";
+>>>>>>> parent of 5ed8feb... hello.c
 	return 0;
 }
